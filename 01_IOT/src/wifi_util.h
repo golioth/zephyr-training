b0ceb694af292c5dc873ff53ce2c35c38ce55b5d
Mike Szczys <mike@golioth.io>
2023-03-27 19:43:19 -0500

    add RPC to fetch WiFi status info
    Calling the `get_wifi_info` remote procedure call will query and return
    the WiFi iface status
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- 01_IOT/src/wifi_util.h
+++ 01_IOT/src/wifi_util.h
@@ -7,6 +7,10 @@
 #ifndef __WIFI_UTIL_H__
 #define __WIFI_UTIL_H__
 
+#include <zephyr/net/wifi_mgmt.h>
+#include "net_private.h"
+
 int wifi_connect(void);
+int cmd_wifi_status(struct wifi_iface_status *status);
 
 #endif
