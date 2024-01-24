d2832418a63a50a109caa235f86a58beed2390bb
Jonathan Beri <jberi@golioth.io>
2024-01-24 12:26:26 -0800

    Moves onCreateCommands to shell script for better readability

---
+++ .devcontainer/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+west init -l app
+west update
+west zephyr-export
+pip install -r deps/zephyr/scripts/requirements.txt
+echo \"alias ll='ls -lah'\" >> $HOME/.bashrc
+west completion bash > $HOME/west-completion.bash
+echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
