b2795cdfd40a786f673c971a172ad7f0b59ff3f0
Mike Szczys <mike@golioth.io>
2024-04-04 17:26:26 -0500

    devcontainer: rename init.sh to onCreateCommand.sh
    - Rename init.sh to onCreateCommand.sh
    - use interactive bash shell to run script
    - fix quotes in script
    - erase history so it doesn't appear on constainer terminal when pressing
      up arrow
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,6 +4,7 @@ west init -l app
 west update
 west zephyr-export
 pip install -r deps/zephyr/scripts/requirements.txt
-echo \"alias ll='ls -lah'\" >> $HOME/.bashrc
+echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+history -c
