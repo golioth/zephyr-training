04eeba6bb4a5caa0e5ed986acb3a9d6b50c99d06
Mike Szczys <michael.szczys@canonical.com>
2026-06-23 17:01:20 -0500

    devcontainer: update for Golioth v0.22.0
    Update devcontainer to match build and initialization with CI builds based
    on golioth-firmware-sdk v0.22.0
    
    Signed-off-by: Mike Szczys <michael.szczys@canonical.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,7 +3,8 @@
 west init -l app
 west update
 west zephyr-export
-pip install -r deps/zephyr/scripts/requirements.txt
+west patch apply
+uv pip install -r deps/zephyr/scripts/requirements.txt
 echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
